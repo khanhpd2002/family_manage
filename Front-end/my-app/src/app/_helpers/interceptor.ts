159227dbf907a9091a2830f1448733a3164b04f4
hangockhanh <kimdungvaech@gmail.com>
2023-01-09 09:18:12 +0700

    fix interceptor for cors provice api

--- Front-end/my-app/src/app/_helpers/interceptor.ts
+++ Front-end/my-app/src/app/_helpers/interceptor.ts
@@ -1,20 +1,28 @@
 import {Injectable} from '@angular/core';
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpErrorResponse} from '@angular/common/http';
+import {BehaviorSubject, Observable} from 'rxjs';
+import { catchError, filter, finalize, switchMap, take } from 'rxjs/operators';
+
 
 @Injectable()
 export class Interceptor implements HttpInterceptor {
   constructor() {
   }
+  private refreshTokenInProgress = false;
+  private refreshTokenSubject = new BehaviorSubject(null);
 
   token = window.sessionStorage.getItem('token');
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    request = request.clone({
+    var request1 = request.clone({
       setHeaders: {
         'Content-Type': 'application/json',
         Authorization: this.token ? `Bearer ${this.token.substring(1, this.token.length - 1)}` : ''
       },
     });
-    return next.handle(request);
+    return next.handle(request1).pipe(
+      catchError((err) => {
+        return next.handle(request);
+      })
+    );
   }
 }
